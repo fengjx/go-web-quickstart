a5a421119fa182a20fc4f63953a95f8b2677afd2
fengjx <fengjianxin2012@gmail.com>
2023-08-20 03:05:19 +0800

    dao & service hook

--- internal/base/dao/init.go
+++ internal/base/dao/init.go
@@ -2,13 +2,14 @@ package dao
 
 import (
 	"github.com/fengjx/go-web-quickstart/internal/app/applog"
+	"github.com/fengjx/go-web-quickstart/internal/app/hook"
 	"github.com/fengjx/go-web-quickstart/internal/base/dao/blog"
 	"github.com/fengjx/go-web-quickstart/internal/base/dao/user"
 )
 
 func Init() {
-	applog.Log.Infof("dao init start")
 	_ = user.GetDao()
 	_ = blog.GetDao()
-	applog.Log.Infof("dao init end")
+	applog.Log.Infof("dao init")
+	hook.OnDaoInit()
 }
--- internal/service/init.go
+++ internal/service/init.go
@@ -1,10 +1,13 @@
 package service
 
-import "github.com/fengjx/go-web-quickstart/internal/app/applog"
+import (
+	"github.com/fengjx/go-web-quickstart/internal/app/applog"
+	"github.com/fengjx/go-web-quickstart/internal/app/hook"
+)
 
 func Init() {
-	applog.Log.Infof("service init start")
 	_ = GetUserSvc()
 	_ = GetBlogSvc()
-	applog.Log.Infof("service init end")
+	applog.Log.Infof("service init")
+	hook.OnServiceInit()
 }
